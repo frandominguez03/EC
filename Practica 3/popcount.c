b012892c5495abe40c44dd39dcc6b61d23aa2bcf
frandominguez03 <d3vcho@protonmail.com>
2018-10-29 15:59:25 +0100

    Actualización. Falla el popcount 5 en el test 4.

--- Practica 3/popcount.c
+++ Practica 3/popcount.c
@@ -26,6 +26,13 @@ ___________________________________________________
 #include <stdlib.h>		// para exit()
 #include <sys/time.h>		// para gettimeofday(), struct timeval
 
+const unsigned long m1 = 0x5555555555555555;
+const unsigned long m2 = 0x3333333333333333;
+const unsigned long m4 = 0x0f0f0f0f0f0f0f0f;
+const unsigned long m8 = 0x00ff00ff00ff00ff;
+const unsigned long m16 = 0x0000ffff0000ffff;
+const unsigned long m32 = 0x00000000ffffffff; //binary: 32 zeros, 32 ones
+
 int resultado=0;
 
 #ifndef TEST
@@ -153,6 +160,172 @@ int popcount4(unsigned* array, size_t len)
     return res;
 }
 
+int popcount5(unsigned* array, size_t len)
+{
+	size_t   x;
+	size_t val=0;
+
+    for (size_t i=0; i<len; i++)
+    {
+    	x = array[i];
+		for (size_t j=0; j<8*sizeof(int); j++) {
+			val += x & 0x01;
+			x >>= 1;
+		}
+
+	val += (val >> 16);
+	val += (val >> 8);
+    }
+
+	return val;
+}
+
+int popcount6(unsigned* array, size_t len)
+{
+	size_t i;
+	unsigned long x;
+	int result = 0;
+
+	for (i=0; i<len; i++)
+	{
+		x = array[i];
+
+		x = (x & m1 ) + ((x >> 1) & m1 );
+		x = (x & m2 ) + ((x >> 2) & m2 ); //put count of each 4 bits into
+		x = (x & m4 ) + ((x >> 4) & m4 ); //put count of each 8 bits into
+		x = (x & m8 ) + ((x >> 8) & m8 ); //put count of each 16 bits into
+		x = (x & m16) + ((x >> 16) & m16); //put count of each 32 bits into
+		x = (x & m32) + ((x >> 32) & m32);
+
+		result += x;
+	}
+
+	return result;
+}
+
+int popcount7(unsigned* array, size_t len)
+{
+	size_t i;
+	unsigned long x1,x2;
+	int result=0;
+
+	if (len & 0x3) printf("leyendo 128b pero len no múltiplo de 4\n");
+
+	for (i=0; i<len; i+=4)
+	{
+		x1 = *(unsigned long*) &array[i ];
+		x2 = *(unsigned long*) &array[i+2];
+
+		x1 = (x1 & m1 ) + ((x1 >> 1) & m1 );
+		x1 = (x1 & m2 ) + ((x1 >> 2) & m2 );
+		x1 = (x1 & m4 ) + ((x1 >> 4) & m4 );
+		x1 = (x1 & m8 ) + ((x1 >> 8) & m8 );
+		x1 = (x1 & m16) + ((x1 >> 16) & m16);
+		x1 = (x1 & m32) + ((x1 >> 32) & m32);
+
+		x2 = (x2 & m1 ) + ((x2 >> 1) & m1 );
+		x2 = (x2 & m2 ) + ((x2 >> 2) & m2 );
+		x2 = (x2 & m4 ) + ((x2 >> 4) & m4 );
+		x2 = (x2 & m8 ) + ((x2 >> 8) & m8 );
+		x2 = (x2 & m16) + ((x2 >> 16) & m16);
+		x2 = (x2 & m32) + ((x2 >> 32) & m32);
+		result+= x1+x2;
+	}
+	return result;
+}
+
+int popcount8(unsigned* array, size_t len)
+{
+	size_t i;
+	int val, result=0;
+	int SSE_mask[] = {0x0f0f0f0f, 0x0f0f0f0f, 0x0f0f0f0f, 0x0f0f0f0f};
+	int SSE_LUTb[] = {0x02010100, 0x03020201, 0x03020201, 0x04030302};
+
+	if (len & 0x3) printf("leyendo 128b pero len no múltiplo de 4\n");
+
+	for (i=0; i<len; i+=4)
+	{
+		asm("movdqu	%[x], %%xmm0\n\t"
+			"movdqa %%xmm0, %%xmm1 \n\t" //x: two copies xmm0-1
+			"movdqu %[m], %%xmm6 \n\t" // mask: xmm6
+			"psrlw $4 , %%xmm1 \n\t"
+			"pand %%xmm6, %%xmm0 \n\t" //; xmm0 – lower nibbles
+			"pand %%xmm6, %%xmm1 \n\t" //; xmm1 – higher nibbles
+
+			"movdqu	%[l], %%xmm2\n\t"
+			"movdqa %%xmm2, %%xmm3 \n\t" //x: two copies xmm0-1
+			"pshufb %%xmm0, %%xmm2 \n\t"
+			"pshufb %%xmm1, %%xmm3 \n\t"
+
+			"paddb %%xmm2, %%xmm3 \n\t"
+			"pxor  %%xmm0, %%xmm0 \n\t"
+			"psadbw %%xmm0, %%xmm3 \n\t"
+			"movhlps %%xmm3, %%xmm0 \n\t"
+			"paddd %%xmm3, %%xmm0 \n\t"
+			"movd  %%xmm0, %[val]"
+
+			: [val] "=r" (val)
+			: [x] "m" (array[i]),
+			  [m] "m" (SSE_mask[0]),
+			  [l] "m" (SSE_LUTb[0])
+		);
+		result += val;
+	}
+
+	return result;
+}
+
+int popcount9(unsigned* array, size_t len)
+{
+	size_t i;
+	unsigned x;
+	int val, result=0;
+
+	for(i=0; i<len; i++)
+	{
+		x = array[i];
+
+		asm("popcnt %[x], %[val]"
+			: [val] "=r" (val)
+			: 	[x]  "r" (x)
+
+			);
+
+		result += val;
+	}
+
+	return result;
+}
+
+int popcount10(unsigned* array, size_t len)
+{
+	size_t i;
+	unsigned long x1, x2;
+	long val;
+	int result = 0;
+
+	if (len & 0x3) printf("leyendo 128b pero len no múltiplo de 4\n");
+
+	for(i=0; i<len; i+=4)
+	{
+		x1 = *(unsigned long*) &array[i ];
+		x2 = *(unsigned long*) &array[i+2];
+
+		asm("popcnt %[x1], %[val] \n\t"
+			"popcnt %[x2], %%rdi \n\t"
+			"add	%%rdi, %[val] \n\t"
+			: [val] "=&r" (val)
+			: 	[x1]  "r" (x1),
+			 	[x2]  "r" (x2)
+
+			);
+
+		result += val;
+	}
+
+	return result;
+}
+
 void crono(int (*func)(), char* msg){
     struct timeval tv1,tv2; 			// gettimeofday() secs-usecs
     long           tv_usecs;			// y sus cuentas
@@ -176,19 +349,19 @@ int main()
     #if TEST==0 || TEST==4
 	size_t i;
 	for (i=0; i<SIZE; i++) // inicializar array
-	lista[i]=i;
+		lista[i]=i;
 	#endif
 
     crono(popcount1, "popcount1 (lenguaje C - 	  for)");
 	crono(popcount2, "popcount2 (lenguaje C - 	while)");
 	crono(popcount3 ,"popcount3 (leng.ASM-body while 4i)");
 	crono(popcount4 ,"popcount4 (leng.ASM-body while 3i)");
-	/*crono(popcount5 ,"popcount5 (CS:APP2e 3.49-group 8b)");
+	crono(popcount5 ,"popcount5 (CS:APP2e 3.49-group 8b)");
 	crono(popcount6 ,"popcount6 (Wikipedia- naive - 32b)");
 	crono(popcount7 ,"popcount7 (Wikipedia- naive -128b)");
 	crono(popcount8 ,"popcount8 (asm SSE3 - pshufb 128b)");
 	crono(popcount9 ,"popcount9 (asm SSE4- popcount 32b)");
-	crono(popcount10,"popcount10(asm SSE4- popcount128b)");*/
+	crono(popcount10,"popcount10(asm SSE4- popcount128b)");
 
 	#if TEST != 0
 		printf("calculado = %d\n", RESULT);
